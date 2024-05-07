33b64b5eed519d0bcdefaea7f5c938da80183a57
unknown <23214290130@ACADEMICO.IESB.BR>
2024-05-07 20:27:05 -0300

    first commit

---
+++ testearray.js
@@ -0,0 +1,47 @@
+
+// let numeros = [1,2,3,4,5];
+// for (const n of numeros){
+//      console.log(n%2 === 0 ? `${n} é par` : `${n} é ímpar`)
+// }
+
+
+// let a = [];
+// for(let i=0; i <=5; i++){
+//     a.push(i)
+// }
+// console.log(a)
+
+
+// let numeros = [1,2,3,4,5]
+// for (const numero of numeros){
+//     console.log(numero)
+// }
+// console.log(numeros);
+
+// for(let i=0 ; i<numeros.length; i++){
+//     console.log(numeros [i]);
+// }
+// for (let i=0 ; i<5 ; i++){
+//     console.log(numeros[i]);
+// }
+
+// let pais = "Brasil"
+// for (const letra of pais){
+// console.log(letra);
+// } 
+
+ 
+// let anoInicial = 1896;
+// let anoFinal = 2020;
+// let semOlim = [1916, 1940, 1944];
+// let anosOlim = []
+//  for (let ano = anoInicial; ano <= anoFinal; ano++){
+//     if (ano % 4 === 0 && !semOlim.includes(ano)){
+//         anosOlim.push(ano);
+
+//     }
+//  }
+
+//  for(let ano of anosOlim) {
+//     console.log(`A Olimpiada de numero ${1+anosOlim.indexOf(ano)} ocorreu no ano ${ano}`)
+//  }
